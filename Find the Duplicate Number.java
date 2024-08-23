6384cc8908a0974cb190153f7c971b9838fa12cf
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-08-23 21:14:12 +0530

    Create Find the Duplicate Number.java

---
+++ Find the Duplicate Number.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/find-the-duplicate-number/description/
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // used linked list cycle concept
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast =nums[fast];
+        }
+        return slow;
+    }
+}
